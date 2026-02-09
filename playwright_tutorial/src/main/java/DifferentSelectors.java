deab0e4eb1665f7bc61a5976472d2bb7d81cbba6
azharjamalaj <87015215+azharjamalaj@users.noreply.github.com>
2026-02-09 14:09:32 +0530

    Different types of selectors example

--- playwright_tutorial/src/main/java/DifferentSelectors.java
+++ playwright_tutorial/src/main/java/DifferentSelectors.java
@@ -14,23 +14,41 @@ public class DifferentSelectors {
 
             page.navigate("https://testautomationpractice.blogspot.com/");
 
-//            This class will only focus on the differnt type of selector in Playwright
+            // This class will only focus on the different type of selector in Playwright
 
-//            1. Using Text locator
+            //  1. Using Text locator
             Locator textLocator = page.locator("text=Udemy Courses");
 
+            //  Verify the text matches the actual
             assertThat(textLocator).hasText("Udemy Courses");
+
+            // Click on the Udemy Course link
             textLocator.click();
 
+            // navigate back to the home page
             page.goBack();
 
-//            getByRole
+            //  Use of has-text
+            // Click on the Playwright Practice link
+            page.locator("a:has-text('PlaywrightPractice')").click();
 
+            //Verify the title of the page
+            assertThat(page).hasTitle("Automation Testing Practice: PlaywrightPractice");
 
-            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions()
-                            .setName("Submit"))
-                    .click();
+            // navigate back to the home page
+            page.goBack();
 
+            //Using CSS selector
+            page.locator("div.form-group input#name").fill("Azhar");
+
+            //Using CSS selector to clear field
+            page.locator("div.form-group input#name").clear();
+
+            //Using Xpath with attribute
+            page.locator("//div//input[@id='email']").fill("azhar@gmail.com");
+
+            //Using Xpath Contains and siblings
+            page.locator("//label[contains(text(), 'Phone')]/following-sibling::input").fill("9090909090");
 
         } catch (Exception e) {
             throw new RuntimeException(e);
