846c1f558743aa542442f6820f8812d7f04a35ee
azharjamalaj <87015215+azharjamalaj@users.noreply.github.com>
2026-02-25 15:41:29 +0530

    Comma Separator and xpath union

--- playwright_tutorial/src/main/java/CommaSepratedExample.java
+++ playwright_tutorial/src/main/java/CommaSepratedExample.java
@@ -1,8 +1,12 @@
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class CommaSepratedExample {
     public static void main(String[] args) {
@@ -11,5 +15,31 @@ public class CommaSepratedExample {
 
         Page page = browser.newPage();
         page.navigate("https://www.orangehrm.com/en/book-a-free-demo");
+        assertThat(page).hasTitle(Pattern.compile("OrangeHRM"));
 
-        List<String> componentOfPage = Arrays.asList();
+        // Using Comma Separator
+        Locator loc = page.locator("a:has-text('Solutions'), a:has-text('Why OrangeHRM'), a:has-text('Resources'), a:has-text('Pricing')");
+
+        loc.waitFor();
+
+        // Validate any of the href is visible on the page
+        assertThat(loc).isVisible();
+
+        // Using OR operator
+        Locator solutions = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Solutions").setExact(true));
+        Locator whyOrangeHRM = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Why OrangeHRM").setExact(true));
+        Locator company = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Company").setExact(true));
+        Locator resources = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Resources").setExact(true));
+
+
+        Locator combined = solutions
+                .or(whyOrangeHRM)
+                .or(company)
+                .or(resources);
+
+        combined.first().waitFor();
+        solutions.waitFor();
+        solutions.click();
+        whyOrangeHRM.click();
+    }
+}
