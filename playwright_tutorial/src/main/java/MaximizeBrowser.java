f9b800bbca7eb616de583687f11942c36aaadf2d
azharjamalaj <87015215+azharjamalaj@users.noreply.github.com>
2026-03-05 12:05:59 +0530

    File download and maximise window

--- playwright_tutorial/src/main/java/MaximizeBrowser.java
+++ playwright_tutorial/src/main/java/MaximizeBrowser.java
@@ -1,22 +1,22 @@
 import com.microsoft.playwright.*;
 
-import java.nio.file.Path;
+import java.awt.*;
 import java.nio.file.Paths;
 
-public class FileDownload {
+public class MaximizeBrowser {
     public static void main(String[] args) {
+        Dimension dimension=Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) dimension.getWidth();
+        int height = (int) dimension.getHeight();
+
+        System.out.println("width" + width+ "and height " + height);
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-
+        BrowserContext context = browser.newContext(new Browser.NewContextOptions().setViewportSize(width, height));
         Page page = browser.newPage();
         page.navigate("https://the-internet.herokuapp.com/download");
 
-        Download download = page.waitForDownload(()->{page.locator("a:text('tmpvnq5ytwm.png')").click();});
 
-        System.out.println(download.url());
-        System.out.println(download.path());
-
-        download.saveAs(Paths.get("download.json"));
         page.close();
         browser.close();
         playwright.close();
